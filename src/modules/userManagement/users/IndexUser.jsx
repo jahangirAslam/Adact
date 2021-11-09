45f711f2053767d9e60b795384420d41ada09c7d
muhammad fayyaz <fayyaz@tv2u.com>
2021-11-09 13:58:30 +0500

    formating

--- src/modules/userManagement/users/IndexUser.jsx
+++ src/modules/userManagement/users/IndexUser.jsx
@@ -1,27 +1,27 @@
-import React, { useState, useEffect } from 'react'
-import { useHistory } from 'react-router-dom'
-import { Tag } from 'antd'
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import { Tag } from 'antd';
 
-import { HeaderComponent, BodyComponent, TableComponent, ActionComponent, CreateComponent } from '@comps/components'
-import { execWithLoadingState, formatCompleteDataTime, notify } from '@utils/helpers'
-import { getUsers, deleteUser } from './requests'
-import CreateUser from './components/CreateUser'
+import { HeaderComponent, BodyComponent, TableComponent, ActionComponent, CreateComponent } from '@comps/components';
+import { execWithLoadingState, formatCompleteDataTime, notify } from '@utils/helpers';
+import { getUsers, deleteUser } from './requests';
+import CreateUser from './components/CreateUser';
 
 const IndexUser = () => {
 
-  const history = useHistory()
-  const [loader, setLoader] = useState('')
+  const history = useHistory();
+  const [loader, setLoader] = useState('');
 
-  const [dataSource, setDataSource] = useState([])
-  const [totalRecords, setTotalRecords] = useState(0)
+  const [dataSource, setDataSource] = useState([]);
+  const [totalRecords, setTotalRecords] = useState(0);
   const [pagination, setPagination] = useState({
     current: 1,
     pageSize: 10,
     sortName: 'id',
     sortType: 'desc'
-  })
+  });
 
-  const [childComponent, setChildComponent] = useState(null)
+  const [childComponent, setChildComponent] = useState(null);
 
   const columns = [
     {
@@ -42,11 +42,11 @@ const IndexUser = () => {
       dataIndex: 'is_active',
       sorter: true,
       render: (is_active) => {
-        let color = is_active ? 'green' : 'red'
-        let text = is_active ? 'ACTIVE' : 'INACTIVE'
+        let color = is_active ? 'green' : 'red';
+        let text = is_active ? 'ACTIVE' : 'INACTIVE';
         return (
           <Tag color={color}>{text}</Tag>
-        )
+        );
       }
     },
     {
@@ -55,7 +55,7 @@ const IndexUser = () => {
       dataIndex: 'created_at',
       sorter: true,
       render: (created_at) => {
-        return formatCompleteDataTime(created_at)
+        return formatCompleteDataTime(created_at);
       }
     },
     {
@@ -63,12 +63,12 @@ const IndexUser = () => {
       title: 'Actions',
       render: (record) => ActionComponent({ each: record, onView: onView, onEdit: onEdit, onDelete: onDelete })
     },
-  ]
+  ];
 
   useEffect(() => {
-    getAllUsers()
+    getAllUsers();
     // eslint-disable-next-line
-  }, [pagination])
+  }, [pagination]);
 
   const getAllUsers = () => {
     let payload = {
@@ -76,19 +76,19 @@ const IndexUser = () => {
       length: pagination.pageSize,
       sort_name: pagination.sortName,
       sort_type: pagination.sortType,
-    }
-    execWithLoadingState(setLoader, getUsers, payload, onSuccess, null)
-  }
+    };
+    execWithLoadingState(setLoader, getUsers, payload, onSuccess, null);
+  };
 
   const onSuccess = (response) => {
-    setTotalRecords(response.recordsTotal)
-    setDataSource(response.data)
-  }
+    setTotalRecords(response.recordsTotal);
+    setDataSource(response.data);
+  };
 
   const onDeleteSuccess = (response, msg) => {
-    setDataSource(dataSource.filter((index) => (index.id !== response.id)))
-    notify(msg.msg)
-  }
+    setDataSource(dataSource.filter((index) => (index.id !== response.id)));
+    notify(msg.msg);
+  };
 
   const handleTableChange = (page, filters, sorter) => {
     let payload = {
@@ -97,32 +97,32 @@ const IndexUser = () => {
       pageSize: page.pageSize,
       sortName: sorter.field || 'id',
       sortType: sorter.order === 'ascend' ? 'asc' : 'desc',
-    }
-    setPagination(payload)
-  }
+    };
+    setPagination(payload);
+  };
 
   // Create component modal
   const onCreate = () => {
-    setChildComponent(<CreateUser onCreated={onCreated}/>)
-  }
+    setChildComponent(<CreateUser onCreated={onCreated}/>);
+  };
   const onCreated = (success) => {
     if (success) {
-      getAllUsers()
+      getAllUsers();
     }
-    setChildComponent(null)
-  }
+    setChildComponent(null);
+  };
 
   const onView = (record) => {
-    history.push(`/user-management/users/view/${record.id}`)
-  }
+    history.push(`/user-management/users/view/${record.id}`);
+  };
 
   const onEdit = (record) => {
-    history.push(`/user-management/users/edit/${record.id}`)
-  }
+    history.push(`/user-management/users/edit/${record.id}`);
+  };
 
   const onDelete = (record) => {
-    execWithLoadingState(setLoader, deleteUser, record.id, onDeleteSuccess, null)
-  }
+    execWithLoadingState(setLoader, deleteUser, record.id, onDeleteSuccess, null);
+  };
 
   return (
     <>
@@ -135,7 +135,7 @@ const IndexUser = () => {
                         pagination={{ ...pagination, total: totalRecords }} onChange={handleTableChange}/>
       </BodyComponent>
     </>
-  )
-}
+  );
+};
 
-export default IndexUser
+export default IndexUser;
