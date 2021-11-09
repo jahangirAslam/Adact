f8b34bc40d49935aa937cc18329db7660e780a22
muhammad fayyaz <fayyaz@tv2u.com>
2021-11-09 13:59:35 +0500

    formating

--- src/modules/userManagement/users/IndexUser.jsx
+++ src/modules/userManagement/users/IndexUser.jsx
@@ -1,66 +1,66 @@
-import React, { useState, useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
-import { Tag } from 'antd';
+import React, { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import { Tag } from "antd";
 
-import { HeaderComponent, BodyComponent, TableComponent, ActionComponent, CreateComponent } from '@comps/components';
-import { execWithLoadingState, formatCompleteDataTime, notify } from '@utils/helpers';
-import { getUsers, deleteUser } from './requests';
-import CreateUser from './components/CreateUser';
+import { HeaderComponent, BodyComponent, TableComponent, ActionComponent, CreateComponent } from "@comps/components";
+import { execWithLoadingState, formatCompleteDataTime, notify } from "@utils/helpers";
+import { getUsers, deleteUser } from "./requests";
+import CreateUser from "./components/CreateUser";
 
 const IndexUser = () => {
 
   const history = useHistory();
-  const [loader, setLoader] = useState('');
+  const [loader, setLoader] = useState("");
 
   const [dataSource, setDataSource] = useState([]);
   const [totalRecords, setTotalRecords] = useState(0);
   const [pagination, setPagination] = useState({
     current: 1,
     pageSize: 10,
-    sortName: 'id',
-    sortType: 'desc'
+    sortName: "id",
+    sortType: "desc"
   });
 
   const [childComponent, setChildComponent] = useState(null);
 
   const columns = [
     {
-      key: 'name',
-      title: 'Name',
-      dataIndex: 'name',
+      key: "name",
+      title: "Name",
+      dataIndex: "name",
       sorter: true,
     },
     {
-      key: 'role_name',
-      title: 'Role',
-      dataIndex: 'role_name',
+      key: "role_name",
+      title: "Role",
+      dataIndex: "role_name",
       sorter: true,
     },
     {
-      key: 'status',
-      title: 'Status',
-      dataIndex: 'is_active',
+      key: "status",
+      title: "Status",
+      dataIndex: "is_active",
       sorter: true,
       render: (is_active) => {
-        let color = is_active ? 'green' : 'red';
-        let text = is_active ? 'ACTIVE' : 'INACTIVE';
+        let color = is_active ? "green" : "red";
+        let text = is_active ? "ACTIVE" : "INACTIVE";
         return (
           <Tag color={color}>{text}</Tag>
         );
       }
     },
     {
-      key: 'created_at',
-      title: 'Create At',
-      dataIndex: 'created_at',
+      key: "created_at",
+      title: "Create At",
+      dataIndex: "created_at",
       sorter: true,
       render: (created_at) => {
         return formatCompleteDataTime(created_at);
       }
     },
     {
-      key: 'actionns',
-      title: 'Actions',
+      key: "actionns",
+      title: "Actions",
       render: (record) => ActionComponent({ each: record, onView: onView, onEdit: onEdit, onDelete: onDelete })
     },
   ];
@@ -95,8 +95,8 @@ const IndexUser = () => {
       ...pagination,
       current: page.current,
       pageSize: page.pageSize,
-      sortName: sorter.field || 'id',
-      sortType: sorter.order === 'ascend' ? 'asc' : 'desc',
+      sortName: sorter.field || "id",
+      sortType: sorter.order === "ascend" ? "asc" : "desc",
     };
     setPagination(payload);
   };
