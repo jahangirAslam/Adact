0ec4b51c33cd486871c19455319b323e1f97acc4
muhammad fayyaz <fayyaz@tv2u.com>
2021-11-10 19:26:35 +0500

    pr review continue...

--- src/wrappers/layout/BaseActions.jsx
+++ src/wrappers/layout/BaseActions.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Button, Popconfirm } from "antd";
-import {DeleteOutlined, EditOutlined, FolderViewOutlined, WarningOutlined} from "@ant-design/icons";
+import {DeleteOutlined, EditOutlined, FolderViewOutlined, WarningOutlined, PlusOutlined} from "@ant-design/icons";
 import "./layouts-styles.css"
 
 const classes = "gx-p-0 gx-my-0 button-padding-5";
@@ -8,8 +8,8 @@ const classes = "gx-p-0 gx-my-0 button-padding-5";
 const BaseAction = (props) => {
   return (
     <>
-      {props.onView ? <Button className={classes} type="link" size="middle" onClick={() => view(props)}><FolderViewOutlined className={"icon-style"}/></Button> : null}
-      {props.onAdd ? <Button className={classes} type="link" size="middle" onClick={() => add(props)}>Add</Button> : null}
+      {props.onView ? <Button className={classes} type="link" size="middle" onClick={() => view(props)}><FolderViewOutlined className={"icon-style"} /></Button> : null}
+      {props.onAdd ? <Button className={classes} type="link" size="middle" onClick={() => add(props)}><PlusOutlined  className={"icon-style"} /></Button> : null}
       {props.onEdit ? <Button className={classes} type="link" size="middle" onClick={() => edit(props)}><EditOutlined className={"icon-style"} /></Button> : null}
       {props.onDelete ? (
         <Popconfirm title="Are you sure?" icon={<WarningOutlined />} onConfirm={() => del(props)}>
