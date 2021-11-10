b80860b5961e7e6921eb8312e366922bcdf57b2d
muhammad fayyaz <fayyaz@tv2u.com>
2021-11-10 14:43:15 +0500

    formating continue...

--- src/wrappers/layout/BaseModal.jsx
+++ src/wrappers/layout/BaseModal.jsx
@@ -1,19 +1,19 @@
-import {Modal, Typography} from "antd";
+import { Modal, Typography } from "antd";
 import React from "react";
 
 
 const BaseModal = (props) => {
 
-    let title = <Typography.Text>{props.mainTitle}</Typography.Text>
-    if (props.subTitle) {
-        title = (
-            <>
-                {title}
-                <Typography.Text type="secondary"> | {props.subTitle}</Typography.Text>
-            </>
-        );
-    }
-    return <Modal width="500px" title={title} {...props} bodyStyle={{padding: "20px"}} >{props.children}</Modal>;
+  let title = <Typography.Text>{props.mainTitle}</Typography.Text>
+   if (props.subTitle) {
+     title = (
+       <>
+         {title}
+         <Typography.Text type="secondary"> | {props.subTitle}</Typography.Text>
+       </>
+     );
+   }
+   return <Modal width="500px" title={title} {...props} bodyStyle={{padding: "20px"}} >{props.children}</Modal>;
 };
 
 export default BaseModal;
