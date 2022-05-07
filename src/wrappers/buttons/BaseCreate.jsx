7a09f9ab73c47165cf24247c527d2afd50a8e4e2
Chand <chandjr0@gmail.com>
2022-05-07 15:30:34 +0500

    styles

--- src/wrappers/buttons/BaseCreate.jsx
+++ src/wrappers/buttons/BaseCreate.jsx
@@ -2,6 +2,6 @@ import React from "react";
 import BaseButton from "./BaseButton";
 import { PlusOutlined } from '@ant-design/icons';
 
-const BaseCreate = (props) => <BaseButton type="primary" className={"button-right-align"} {...props} >Create <PlusOutlined /></BaseButton>
+const BaseCreate = (props) => <BaseButton type="primary" className={"button-right-align"} {...props} >ADD <PlusOutlined /></BaseButton>
 
 export default BaseCreate;
