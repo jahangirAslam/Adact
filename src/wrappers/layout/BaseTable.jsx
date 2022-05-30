c3bae1898abb4e388883b4d49913ec2cf5214bfa
Chand <chandjr0@gmail.com>
2022-05-30 17:39:49 +0500

    del multiple

--- src/wrappers/layout/BaseTable.jsx
+++ src/wrappers/layout/BaseTable.jsx
@@ -1,11 +1,33 @@
-import { Table } from "antd";
-import React, { useMemo } from "react";
+import React, { useMemo, useState } from 'react';
+import { Table, Radio, Divider } from 'antd';
 
-
+const rowSelection = {
+  onChange: (selectedRowKeys, selectedRows) => {
+    console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
+  },
+  getCheckboxProps: (record) => ({
+    disabled: record.name === 'Disabled User',
+    // Column configuration not to be checked
+    name: record.name,
+  }),
+};
 const BaseTable = (props) => {
   let { columns, loader, ...otherProps } = props;
 
-  const tab = useMemo(() => <Table rowKey="id" size="small" {...otherProps} columns={columns} showSorterTooltip={false} tableLayout="fixed" loading={loader} />, [otherProps.dataSource, loader]);// eslint-disable-line react-hooks/exhaustive-deps
+  const tab = useMemo(() => <div>
+
+
+    <Table
+      rowKey="id" size="small" {...otherProps}
+      rowSelection={{
+
+        ...rowSelection,
+      }}
+      columns={columns}
+      dataSource={props.dataSource}
+      loading={loader}
+    />
+  </div>)
   return tab;
 };
 
