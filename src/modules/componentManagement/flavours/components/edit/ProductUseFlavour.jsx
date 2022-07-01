1ea087414884259a5750fa2f39acdc7b7db5b3ca
jahangirAslam <hafiz.j.aslam@gmail.com>
2022-07-01 11:38:55 +0500

    list of product which uses this flavour

---
+++ src/modules/componentManagement/flavours/components/edit/ProductUseFlavour.jsx
@@ -0,0 +1,216 @@
+import { ActionComponent, CreateButton, EditAbleTable } from "@comps/components";
+import { makeRequest, notify, removeById } from "@utils/helpers";
+import { Form, Popconfirm, Row, Typography } from "antd";
+import { default as React, useEffect, useState } from "react";
+import CreateRecipe from "./recipe/CreateRecipe";
+import { deleteFlavour, getFlavours, updateSubstance } from "./recipe/request";
+const originData = [];
+const ProductUseFlavour = (props) => {
+    var delItems = []
+    const [loader, setLoader] = useState(false);
+    const [form] = Form.useForm();
+    const [dataSource, setDataSource] = useState([]);
+    const [availblePercentage, setAvailblePercentage] = useState([]);
+    const [totalRecords, setTotalRecords] = useState(0);
+    const [data, setData] = useState(originData);
+    const [pagination, setPagination] = useState({
+        current: 1,
+        pageSize: 10,
+        sortName: 'id',
+        sortType: 'desc'
+    });
+
+    const [childComponent, setChildComponent] = useState(null);
+    const [editingKey, setEditingKey] = useState('');
+
+    const isEditing = (record) => record && record.id === editingKey;
+
+    const edit = (record) => {
+        form.setFieldsValue({
+            ...record,
+        });
+        setEditingKey(record.id);
+
+    };
+
+    const cancel = () => {
+        setEditingKey('');
+    };
+    const save = async (id) => {
+
+        try {
+            const row = await form.validateFields();
+            // const newData = [...data];
+            let payload = {
+                id: id,
+                flavour_id: props.flavourId,
+                percentage: row.percentage,
+
+            }
+            makeRequest(setLoader, updateSubstance, payload, onError);
+            setEditingKey('');
+            getAllFlavours();
+        } catch (errInfo) {
+            console.log('Validate Failed:', errInfo);
+        }
+    };
+
+    const columns = [
+        {
+            title: 'Product Id',
+            dataIndex: 'name',
+            width: '25%',
+            editable: false,
+        },
+        {
+            title: 'Client',
+            dataIndex: 'type',
+            width: '15%',
+            editable: false,
+        },
+        {
+            title: 'Name',
+            dataIndex: 'cas_number',
+            width: '15%',
+            editable: false,
+        },
+        {
+            title: 'On Markit',
+            dataIndex: 'ref',
+            width: '15%',
+            editable: false,
+        },
+        {
+            title: 'Withdrawn',
+            dataIndex: 'percentage',
+            width: '25%',
+            editable: true,
+        },
+        {
+            title: 'type',
+            dataIndex: 'type',
+            width: '25%',
+            editable: true,
+        },
+        {
+            title: 'Insight',
+            dataIndex: 'insights',
+            width: '15%',
+            editable: false,
+        },
+        // {
+        //     title: 'operation',
+        //     dataIndex: 'operation',
+        //     render: (_, record) => {
+        //         const editable = isEditing(record);
+        //         return editable ? (
+        //             <span>
+        //                 <Typography.Link
+        //                     onClick={() => save(record.id)}
+        //                     style={{
+        //                         marginRight: 8,
+        //                     }}
+        //                 >
+        //                     Save
+        //                 </Typography.Link>
+        //                 <Popconfirm title="Sure to cancel?" onConfirm={cancel}>
+        //                     <p>Cancel</p>
+        //                 </Popconfirm>
+        //             </span>
+        //         ) : (
+        //             <ActionComponent each={record} onEdit={edit} onDelete={onDelete} >
+        //             </ActionComponent>
+        //         );
+        //     },
+        // },
+    ];
+
+
+    useEffect(() => {
+        getAllFlavours();
+        //eslint-disable-next-line
+    }, [pagination]);
+
+    const getAllFlavours = () => {
+        let payload = {
+            start: pagination.current - 1,
+            length: pagination.pageSize,
+            sort_name: pagination.sortName,
+            sort_type: pagination.sortType,
+
+        };
+        makeRequest(setLoader, getFlavours, payload, onSuccess, null);
+    }
+
+
+
+    const onSuccess = (response) => {
+        setTotalRecords(response.recordsTotal);
+        setDataSource(response.data);
+        const some = { availValue: 100 - response.data.reduce((n, { percentage }) => n + percentage, 0) }
+        setAvailblePercentage(some);
+    }
+
+    
+
+    const handleTableChange = (page, fil, sorter) => {
+        let payload = {
+            ...pagination,
+            current: page.current,
+            pageSize: page.pageSize,
+            sortName: sorter.field || 'id',
+            sortType: sorter.order === 'ascend' ? 'asc' : 'desc',
+        };
+        setPagination(payload);
+    }
+    const rowSelection = {
+        onChange: (selectedRowKeys) => {
+            delItems=[]
+             delItems=selectedRowKeys
+        },
+    };
+    // Create component modal
+    const onCreate = () => {
+        setChildComponent(<CreateRecipe onCreated={onCreated} flavourID={props.flavourId} availblePercentage={availblePercentage} />);
+    }
+
+    const onCreated = (each) => {
+        if (!each) {
+            setChildComponent(null);
+        }
+        getAllFlavours();
+    }
+   
+    const onDelete = (record) => {
+        let index = delItems.findIndex(o => o === record.id);
+        if(index === -1){
+            delItems.push(record.id)
+        }
+         const payload = {"ids": delItems};
+         makeRequest(setLoader, deleteFlavour, payload, onDeleteSuccess,onError)
+    }
+
+    const onDeleteSuccess = (response, msg) => {
+        getAllFlavours();
+        notify(msg.msg)
+    }
+
+    const onError = (error, msg) => {
+        //
+    }
+
+    return (
+        < >
+            {childComponent}
+            {/* <Row justify="end" className="da-pb-24" >
+                <CreateButton onClick={onCreate} />
+            </Row> */}
+
+            <EditAbleTable loader={loader} columns={columns} dataSource={dataSource} pagination={{ ...pagination, total: totalRecords }} onChange={handleTableChange} isEditAble={true} isEditing={isEditing} form={form} cancel={cancel} rowSelection={rowSelection} />
+        </>
+    );
+}
+
+export default ProductUseFlavour;
+
+
