27060661eddbe7f3bab50db1ecbf9e3ff08b95a3
Chand <chandjr0@gmail.com>
2022-07-05 12:49:50 +0500

    carts

--- src/modules/dashboard/components/Dashboard.jsx
+++ src/modules/dashboard/components/Dashboard.jsx
@@ -1,9 +1,30 @@
 import React from "react";
+import { Chart } from 'react-charts'
 
 const Dashboard = () => {
+  const data = [
+    {
+      label: 'Series 1',
+      data: [[0, 1], [1, 2], [2, 4], [3, 2], [4, 7]]
+    },
+    {
+      label: 'Series 2',
+      data: [[0, 3], [1, 1], [2, 5], [3, 6], [4, 4]]
+    }
+  ]
+  const axes = [
+    { primary: true, type: 'linear', position: 'bottom' },
+    { type: 'linear', position: 'left' }
+  ]
+
   return (
-    <div>
-      hello from dashboard
+    <div
+      style={{
+        width: '100%',
+        height: '300px'
+      }}
+    >
+      <Chart data={data} axes={axes} />
     </div>
   )
 }
