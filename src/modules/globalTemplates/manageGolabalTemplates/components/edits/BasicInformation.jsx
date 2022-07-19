8ba3da207a98bdc30c91fd22ed042ecbf5d2186a
jahangirAslam <hafiz.j.aslam@gmail.com>
2022-07-19 13:31:11 +0500

    template basic information

--- src/modules/globalTemplates/manageGolabalTemplates/components/edits/BasicInformation.jsx
+++ src/modules/globalTemplates/manageGolabalTemplates/components/edits/BasicInformation.jsx
@@ -3,6 +3,7 @@ import { Form, Input, Row, Col, Divider, Switch, Select } from "antd";
 import { makeRequest, getErrorProps, notify } from "@utils/helpers";
 import { updateChemicalCompound } from "../../requests";
 import { ButtonComponent } from "@comps/components";
+import TextArea from "antd/lib/input/TextArea";
 
 
 const BasicInformation = (props) => {
@@ -23,6 +24,9 @@ const BasicInformation = (props) => {
     errorList["password"] = err;
     setErrors(errorList);
   };
+  const onChange = (checked) => {
+    console.log(`switch to ${checked}`);
+  };
 
   // const onChange = (e) => {
   //   console.log("radio checked", e.target.value);
@@ -37,62 +41,110 @@ const BasicInformation = (props) => {
       onFinish={onFinish}
     >
       <Row gutter={[16, 24]}>
-        <Col className="gutter-row" xs={24} md={12} lg={8} >
+        <Col className="gutter-row" xs={24} md={12} lg={12}  >
+          <h3 className="TemplateHeader">Subject </h3>
           <Form.Item
-            name="name"
+            name="title"
             rules={rules.name}
-            label="Name :"
+            label="Test :"
             {...getErrorProps(errors["name"])}
           >
             <Input />
           </Form.Item>
-        </Col>
-        <Col className="gutter-row" xs={24} md={12} lg={8}>
-          <Form.Item name="fed_uuin" label="fed_uuin :">
-            <Input />
+          <Form.Item
+            name="name"
+            rules={rules.name}
+            label="Description :"
+            {...getErrorProps(errors["name"])}
+          >
+            <TextArea className="templateDiscription" rows={4} placeholder="Description" maxLength={200} />
           </Form.Item>
-        </Col>
-        <Col className="gutter-row" xs={24} md={12} lg={8}>
-          <Form.Item name="manufacturer_ref" label="Manufacturer Name :">
-            <Input />
+          <h5 className="TemplateHeader">Master File</h5>
+          <Form.Item name="fed_uuin" label="Assign to table of Content :">
+          <Select />
           </Form.Item>
-        </Col>
-        <Col className="gutter-row" xs={24} md={12} lg={8}>
-          <Form.Item name="manufacturer_ref" label="manufacturer_ref :">
-            <Input />
+          <h5 className="TemplateHeader">Content</h5>
+          {/* text Editor */}
+          <h3 className="TemplateHeader">Status </h3>
+          <Form.Item name="fed_uuin" label=" Update On:">
+            <Input disabled />
           </Form.Item>
-        </Col>
-        <Col className="gutter-row" xs={24} md={12} lg={8}>
-          <Form.Item name="has_recipe" label="Has Recipe :">
-            <Input disabled={true} />
+          <Form.Item name="fed_uuin" label=" Updated:">
+            <Input disabled />
           </Form.Item>
-        </Col>
-        <Col className="gutter-row" xs={24} md={12} lg={8}>
-          <Form.Item name="is_valid_manufacturer" label="The manufacturer records are valid?">
-            <Input disabled={true} />
+          <Form.Item name="fed_uuin" label="Validation Status :">
+          <Select />
           </Form.Item>
-        </Col>
-        <Col className="gutter-row" xs={24} md={12} lg={8}>
-          <Form.Item name="is_valid_manufacturer" label="Recipe is valid">
-            <Input disabled={true} />
+          <Form.Item name="fed_uuin" label="Validate on :">
+            <Input disabled />
           </Form.Item>
-        </Col>
-      </Row>
-      <Divider orientation="left" />
-      <Row gutter={[16, 24]}>
-        <Col className="gutter-row" span={8}>
-          <Form.Item name="is_active" label="Flavour status">
-            <Switch defaultChecked={props.data.is_safe} />
+          <Form.Item name="fed_uuin" label="Validated By :">
+            <Input disabled />
           </Form.Item>
-        </Col>
 
-        <Col className="gutter-row" span={8}>
-          <Form.Item name="is_test" label="Test Mode :">
-            <Switch defaultChecked={props.data.is_test} />
+
+
+        </Col>
+        <Col className="gutter-row" xs={24} md={12} lg={11} offset={1}>
+        <h3 className="TemplateHeader">Report Level </h3>
+          <Form.Item name="fed_uuin" label="Overview :">
+            <Input />
+          </Form.Item>
+          <h5 className="TemplateHeader">Apply This Templates To Specified Product Type</h5>
+          <Form.Item name="fed_uuin" label="Not Set :">
+            <Select />
+          </Form.Item>
+          <h5 className="TemplateHeader">Document Visibility</h5>
+          <Form.Item name="fed_uuin" label="Not Confidental :">
+          <Switch defaultChecked onChange={onChange} />;
+
+          </Form.Item>
+          <Form.Item name="fed_uuin" label="Public :">
+          <Switch defaultChecked onChange={onChange} />
+          </Form.Item>
+          <h5 className="TemplateHeader">Template For</h5>
+          <Form.Item name="fed_uuin" label="Specified Customers :">
+          <Switch defaultChecked onChange={onChange} />
+
+          </Form.Item>
+          <h5 className="TemplateHeader">Document Type</h5>
+          <Form.Item name="fed_uuin" label="TPD Document :">
+          <Switch defaultChecked onChange={onChange} />
+
+          </Form.Item>
+          <Form.Item name="fed_uuin" label="PMTA Document :">
+          <Switch defaultChecked onChange={onChange} />
+
+          </Form.Item>
+          <Form.Item name="fed_uuin" label="MHRA Document :">
+          <Switch defaultChecked onChange={onChange} />
+
+          </Form.Item>
+          <Form.Item name="fed_uuin" label="CBD Document :">
+          <Switch defaultChecked onChange={onChange} />
+
+          </Form.Item>
+          <h5 className="TemplateHeader">Default For</h5>
+          <Form.Item name="fed_uuin" label="TPD  :">
+          <Switch defaultChecked onChange={onChange} />
+
+          </Form.Item>
+          <Form.Item name="fed_uuin" label="PMTA  :">
+          <Switch defaultChecked onChange={onChange} />
+
+          </Form.Item>
+          <Form.Item name="fed_uuin" label="MHRA  :">
+          <Switch defaultChecked onChange={onChange} />
+
+          </Form.Item>
+          <Form.Item name="fed_uuin" label="CBD  :">
+          <Switch defaultChecked onChange={onChange} />
+
           </Form.Item>
         </Col>
+        
       </Row>
-      <Divider orientation="left" />
+      
 
       <Form.Item style={{ textAlign: "end" }}>
         <ButtonComponent
