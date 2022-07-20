ea7ecdbd347fc8c61a62bc3ffb078ae3bee9db79
Chand <chandjr0@gmail.com>
2022-07-20 13:58:26 +0500

    commt

--- src/modules/dashboard/components/Dashboard.jsx
+++ src/modules/dashboard/components/Dashboard.jsx
@@ -1,44 +1,67 @@
 import { CChart } from '@coreui/react-chartjs';
+import { makeRequest } from "@utils/helpers";
 import { Switch } from "antd";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { getDashbordData } from './requests';
+
 const Dashboard = () => {
+  // let lablesData = []
   const [disabled, setDisabled] = useState(true);
+  const [lablesData, setLablesData] = useState([]);
+  const [dataSource, setDataSource] = useState([]);
+  const [loader, setLoader] = useState(false);
+
   const toggle = () => {
     setDisabled(!disabled);
   };
-  const data = {
-    labels: ["January", "February", "March", "April", "May", "June", "July"],
-    datasets: [
-      {
-        label: "My First dataset",
-        backgroundColor: "rgba(220, 220, 220, 0.2)",
-        borderColor: "rgba(220, 220, 220, 1)",
-        pointBackgroundColor: "rgba(220, 220, 220, 1)",
-        pointBorderColor: "#fff",
-        data: [40, 20, 12, 39, 10, 40, 39, 80, 40]
-      },
-      {
-        label: "My Second dataset",
-        backgroundColor: "rgba(151, 187, 205, 0.2)",
-        borderColor: "rgba(151, 187, 205, 1)",
-        pointBackgroundColor: "rgba(151, 187, 205, 1)",
-        pointBorderColor: "#fff",
-        data: [50, 12, 28, 29, 7, 25, 12, 70, 60]
-      },
-    ],
+  useEffect(() => {
+    getData()
+  }, [])
+  const getData = () => {
+    let payload = {}
+    makeRequest(setLoader, getDashbordData, payload, onSuccess, null);
+
   }
+  const onSuccess = (response) => {
+    let lables = []
+    let data = []
+    response.forEach(element => {
+      data.push(element.value);
+      lables.push(element.month)
+      
+    });
+    setLablesData(lables)
+    setDataSource(data)
+  }
+
+
   return (
     <div>
       <div className="dashboardBtn">
-      <Switch className="dashboardswitch" checkedChildren="Monthly" unCheckedChildren="Yearly" />
-
+        <Switch className="dashboardswitch" checkedChildren="Monthly" unCheckedChildren="Yearly" />
       </div>
-    <CChart
-    className="dashboardChart"
-      type="line"
-      data={data}
-    />
-</div>
+    
+        <CChart
+          className="dashboardChart"
+          type="line"
+          data={{
+            labels: lablesData,
+            datasets: [
+              {
+                label: "My First dataset",
+                backgroundColor: "rgba(220, 220, 220, 0.2)",
+                borderColor: "rgba(220, 220, 220, 1)",
+                pointBackgroundColor: "rgba(220, 220, 220, 1)",
+                pointBorderColor: "#fff",
+                data: dataSource
+              },
+
+            ],
+          }}
+
+        />
+     
+    </div>
   )
 }
 export default Dashboard;
