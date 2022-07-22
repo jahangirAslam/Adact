aae7749da41a3f7e91de0ecf426ff9ad3d6c1501
Chand <chandjr0@gmail.com>
2022-07-22 10:57:05 +0500

    commt

--- src/containers/components/header/HeaderUser.jsx
+++ src/containers/components/header/HeaderUser.jsx
@@ -1,11 +1,11 @@
-import React from "react";
-import { useDispatch, useSelector } from "react-redux";
-
-import { Menu, Dropdown, Col, Avatar } from "antd";
-import { User, Logout } from "react-iconly";
-import { logout as logoutAction } from "@mods/userManagement/auth/authSlice";
 import Man from "@assets/images/menu/man.svg";
+import { logout as logoutAction } from "@mods/userManagement/auth/authSlice";
+import { Avatar, Col, Dropdown, Menu, Modal } from "antd";
+import { React, useState } from "react";
+import { Logout, User } from "react-iconly";
+import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
+import Profile from "./profile";
 
 const AvatarIcon = () => {
   return <img src={Man} alt="Avatar" />;
@@ -14,16 +14,39 @@ export default function HeaderUser() {
   const history = useHistory();
   const dispatch = useDispatch();
   const user = useSelector((state) => state.auth.authUser);
-
+  const [childComponent, setChildComponent] = useState(null);
+  const [visible, setVisible] = useState(false);
+  const [confirmLoading, setConfirmLoading] = useState(false);
+  const [modalText, setModalText] = useState('Content of the modal');
   const logout = () => {
+
+
     dispatch({ type: logoutAction.type });
     history.push('/');
   };
+  const showModal = () => {
+    setVisible(true);
+  };
 
+  const handleOk = () => {
+    setModalText('The modal will be closed after two seconds');
+    setConfirmLoading(true);
+    setTimeout(() => {
+      setVisible(false);
+      setConfirmLoading(false);
+    }, 2000);
+  };
+
+  const handleCancel = () => {
+    console.log('Clicked cancel button');
+    setVisible(false);
+  };
   const menu = (
+
     <Menu>
       <Menu.Item
         key="user"
+        onClick={showModal}
         icon={
           <User
             set="curved"
@@ -49,13 +72,26 @@ export default function HeaderUser() {
         Logout
       </Menu.Item>
     </Menu>
+
+
   );
 
   return (
-    <Dropdown overlay={menu}>
-      <Col className="da-d-flex-center" onClick={(e) => e.preventDefault()}>
-        <Avatar src={user.profile_url} icon={<AvatarIcon />} size={40} />
-      </Col>
-    </Dropdown>
+    <>
+      <Dropdown overlay={menu}>
+        <Col className="da-d-flex-center" onClick={(e) => e.preventDefault()}>
+          <Avatar src={user.profile_url} icon={<AvatarIcon />} size={40} />
+        </Col>
+      </Dropdown>
+      <Modal
+        title="Profile"
+        visible={visible}
+        onOk={handleOk}
+        confirmLoading={confirmLoading}
+        onCancel={handleCancel}
+      >
+        <Profile />
+      </Modal>
+    </>
   );
 }
