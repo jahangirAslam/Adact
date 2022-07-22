352bb61aab504bf23973a9f45774b30ef00b150b
jahangirAslam <hafiz.j.aslam@gmail.com>
2022-07-22 14:42:41 +0500

    product testing

--- src/modules/products/allProducts/IndexProducts.jsx
+++ src/modules/products/allProducts/IndexProducts.jsx
@@ -1,247 +1,254 @@
-import { Col, Row } from 'antd';
 import { CheckOutlined, CloseOutlined } from "@ant-design/icons";
-import { ActionComponent, BodyComponent, CreateButton, FilterComponent, HeaderComponent, SelectionTable } from "@comps/components";
-import { makeRequest, notify, removeById } from "@utils/helpers";
+import {
+  ActionComponent,
+  BodyComponent,
+  CreateButton,
+  FilterComponent,
+  HeaderComponent,
+  SelectionTable,
+} from "@comps/components";
+import { makeRequest, notify } from "@utils/helpers";
+import { Col, Row } from "antd";
 import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 import CreateProduct from "./components/CreateProducts";
-import { deleteProduct, getAllProducts, getFilters } from "./components/request";
+import {
+  deleteProduct,
+  getAllProducts,
+  getFilters,
+} from "./components/request";
 
 const pageConfig = {
-    headers: {
-        title: "Products list/All Types",
-        breadcrumb: [
-            {
-                name: "All Products",
-            }
-        ]
-    }
-}
+  headers: {
+    title: "Products list/All Types",
+    breadcrumb: [
+      {
+        name: "All Products",
+      },
+    ],
+  },
+};
 
 const IndexProducts = () => {
-    var delItems = []
-    const [loader, setLoader] = useState(false);
-    const history = useHistory();
-    const [filters, setFilters] = useState({});
-    const [dataSource, setDataSource] = useState([]);
-    const [totalRecords, setTotalRecords] = useState(0);
-    const [childComponent, setChildComponent] = useState(null);
-    const [pagination, setPagination] = useState({
-        current: 1,
-        pageSize: 25,
-        sortName: 'id',
-        sortType: 'desc'
-    });
+  var delItems = [];
+  const [loader, setLoader] = useState(false);
+  const history = useHistory();
+  const [filters, setFilters] = useState({});
+  const [dataSource, setDataSource] = useState([]);
+  const [totalRecords, setTotalRecords] = useState(0);
+  const [childComponent, setChildComponent] = useState(null);
+  const [pagination, setPagination] = useState({
+    current: 1,
+    pageSize: 25,
+    sortName: "id",
+    sortType: "desc",
+  });
 
-    const columns = [
+  const columns = [
+    {
+      key: "id",
+      title: "Product ID",
+      dataIndex: "id",
+      sorter: true,
+    },
+    {
+      key: "customer_name",
+      title: "Client Name",
+      dataIndex: "customer_name",
+      sorter: true,
+    },
 
-        {
-            key: 'id',
-            title: 'Product ID',
-            dataIndex: 'id',
-            sorter: true,
-        },
-        {
-            key: 'customer_name',
-            title: 'Client Name',
-            dataIndex: 'customer_name',
-            sorter: true,
-        },
+    {
+      key: "name",
+      title: "Name",
+      dataIndex: "name",
+      sorter: true,
+    },
+    // {
+    //     key: "on_market",
+    //     title: "On Market ",
+    //     dataIndex: "on_market",
+    //     sorter: true,
+    //   },
 
-        {
-            key: 'name',
-            title: 'Name',
-            dataIndex: 'name',
-            sorter: true,
-        },
-        // {
-        //     key: "on_market",
-        //     title: "On Market ",
-        //     dataIndex: "on_market",
-        //     sorter: true,
-        //   },
-     
-        {
-            key: 'withdrawn',
-            title: 'Withdrawn',
-            dataIndex: 'withdrawn',
-            sorter: true,
-        },
-        
-        {
-          key: "type",
-          title: " Type",
-          dataIndex: "type",
-          sorter: true,
-        },
-        
-     
+    {
+      key: "withdrawn",
+      title: "Withdrawn",
+      dataIndex: "withdrawn",
+      sorter: true,
+    },
 
+    {
+      key: "type",
+      title: " Type",
+      dataIndex: "type",
+      sorter: true,
+    },
 
-        {
-            key: "actions",
-            title: 'Actions',
-            render: (record) => ActionComponentEx(record)
-        },
-    ];
+    {
+      key: "actions",
+      title: "Actions",
+      render: (record) => ActionComponentEx(record),
+    },
+  ];
 
-    const ActionComponentEx = (record) => {
-        let icon = null;
-        if (record) {
-            if (record.is_active) {
-                icon = <CloseOutlined className="icon-style da-text-color-danger-1" />;
-            } else {
-                icon = <CheckOutlined className="icon-style da-text-color-success-1" />;
-            }
-        }
-        return (
-            <ActionComponent each={record} onView={onView} onEdit={onEdit} onDelete={onDelete}>
-            </ActionComponent>
-        );
+  const ActionComponentEx = (record) => {
+    let icon = null;
+    if (record) {
+      if (record.is_active) {
+        icon = <CloseOutlined className="icon-style da-text-color-danger-1" />;
+      } else {
+        icon = <CheckOutlined className="icon-style da-text-color-success-1" />;
+      }
     }
-
-
-    useEffect(() => {
-        getProducts();
-        // eslint-disable-next-line
-    }, [pagination, filters]);
-
-    const getProducts = () => {
-        let payload = {
-            start: pagination.current - 1,
-            length: pagination.pageSize,
-            sort_name: pagination.sortName,
-            sort_type: pagination.sortType,
-            filters
-        };
-        makeRequest(setLoader, getAllProducts, payload, onSuccess, null);
-    }
-
-    const onSuccess = (response) => {
-        setTotalRecords(response.recordsTotal);
-        setDataSource(response.data);
-    }
-    const handleTableChange = (page, fil, sorter) => {
-        let payload = {
-            ...pagination,
-            current: page.current,
-            pageSize: page.pageSize,
-            sortName: sorter.field || 'id',
-            sortType: sorter.order === 'ascend' ? 'asc' : 'desc',
-        };
-        setPagination(payload);
-    }
-
-    //deleted multi Items
-    const rowSelection = {
-        onChange: (selectedRowKeys) => {
-            delItems = []
-            delItems = selectedRowKeys
-        },
-    };
-
-
-
-    // Create component modal
-    const onCreate = () => {
-        setChildComponent(<CreateProduct onCreated={onCreated} />)
-    }
-
-    const onCreated = (res) => {
-        if(res){
-            history.push(`/products/product/edit/${res.data.object.id}`);
-        }
-        setChildComponent(null);
-    }
-    const onView = (record) => {
-        history.push(`/products/product/view/${record.id}`);
-
-    }
-    const onEdit = (record) => {
-        history.push(`/products/product/edit/${record.id}`);
-    }
-
-    const onDelete = (record) => {
-        let index = delItems.findIndex(o => o === record.id);
-        if (index === -1) {
-            delItems.push(record.id)
-        }
-        const payload = { "ids": delItems };
-        makeRequest(setLoader, deleteProduct, payload, onDeleteSuccess, onError)
-    }
-
-    const onDeleteSuccess = (response, msg) => {
-        getProducts();
-        notify(msg.msg)
-    }
-
-    const onError = (error, msg) => {
-        notify(msg.message)
-    }
-
     return (
-        <>
-            {childComponent}
-            <HeaderComponent headers={pageConfig.headers}>
-                <CreateButton onClick={onCreate} />
-            </HeaderComponent>
-            <BodyComponent>
-                <FilterComponent filters={availableFilters} onFilter={setFilters} api={getFilters}  />
-                <Row>
-                <Col md={24}>
-                <SelectionTable className="table-show" loader={loader} columns={columns} dataSource={dataSource} rowSelection={rowSelection} onChange={handleTableChange} />
-                </Col>
-                </Row>
-            </BodyComponent>
-        </>
+      <ActionComponent
+        each={record}
+        onView={onView}
+        onEdit={onEdit}
+        onDelete={onDelete}
+      ></ActionComponent>
     );
-}
+  };
+
+  useEffect(() => {
+    getProducts();
+    // eslint-disable-next-line
+  }, [pagination, filters]);
+
+  const getProducts = () => {
+    let payload = {
+      start: pagination.current - 1,
+      length: pagination.pageSize,
+      sort_name: pagination.sortName,
+      sort_type: pagination.sortType,
+      filters,
+    };
+    makeRequest(setLoader, getAllProducts, payload, onSuccess, null);
+  };
+
+  const onSuccess = (response) => {
+    setTotalRecords(response.recordsTotal);
+    setDataSource(response.data);
+  };
+  const handleTableChange = (page, fil, sorter) => {
+    let payload = {
+      ...pagination,
+      current: page.current,
+      pageSize: page.pageSize,
+      sortName: sorter.field || "id",
+      sortType: sorter.order === "ascend" ? "asc" : "desc",
+    };
+    setPagination(payload);
+  };
+
+  //deleted multi Items
+  const rowSelection = {
+    onChange: (selectedRowKeys) => {
+      delItems = [];
+      delItems = selectedRowKeys;
+    },
+  };
+
+  // Create component modal
+  const onCreate = () => {
+    setChildComponent(<CreateProduct onCreated={onCreated} />);
+  };
+
+  const onCreated = (res) => {
+    if (res) {
+      history.push(`/products/product/edit/${res.data.object.id}`);
+    }
+    setChildComponent(null);
+  };
+  const onView = (record) => {
+    history.push(`/products/product/view/${record.id}`);
+  };
+  const onEdit = (record) => {
+    history.push(`/products/product/edit/${record.id}`);
+  };
+
+  const onDelete = (record) => {
+    let index = delItems.findIndex((o) => o === record.id);
+    if (index === -1) {
+      delItems.push(record.id);
+    }
+    const payload = { ids: delItems };
+    makeRequest(setLoader, deleteProduct, payload, onDeleteSuccess, onError);
+  };
+
+  const onDeleteSuccess = (response, msg) => {
+    getProducts();
+    notify(msg.msg);
+  };
+
+  const onError = (error, msg) => {
+    notify(msg.message);
+  };
+
+  return (
+    <>
+      {childComponent}
+      <HeaderComponent headers={pageConfig.headers}>
+        <CreateButton onClick={onCreate} />
+      </HeaderComponent>
+      <BodyComponent>
+        <FilterComponent
+          filters={availableFilters}
+          onFilter={setFilters}
+          api={getFilters}
+        />
+        <Row>
+          <Col md={24}>
+            <SelectionTable
+              className="table-show"
+              loader={loader}
+              columns={columns}
+              dataSource={dataSource}
+              rowSelection={rowSelection}
+              onChange={handleTableChange}
+            />
+          </Col>
+        </Row>
+      </BodyComponent>
+    </>
+  );
+};
 
 export default IndexProducts;
 
 const availableFilters = [
-    {
-        key: 'prduct_id',
-        placeholder: 'Product ID',
-        type: 'text',
-    },
-    {
-        key: 'customer_id',
-        placeholder: 'Client Name',
-        type: 'select',
-        data_key: 'customers',
-
-    },
-    {
-        key: 'name',
-        placeholder: 'Name',
-        type: 'text',
-    },
-    // {
-    //     key: 'name',
-    //     placeholder: 'On Market',
-    //     type: 'text',
-    // },
-    {
-        key: 'withdrawn',
-        placeholder: 'Withdrawn',
-        type: 'select',
-        data_key: 'withdrawn',
-    },
-    {
-        key: 'type',
-        placeholder: 'Type',
-        type: 'select',
-        data_key: 'type',
-
-    },
-
-    // {
-    //     key: 'insights',
-    //     placeholder: 'Insights',
-    //     type: 'select',
-    //     data_key: 'insights',
-
-    // },
-
+  {
+    key: "prduct_id",
+    placeholder: "Product ID",
+    type: "text",
+  },
+  {
+    key: "customer_id",
+    placeholder: "Client Name",
+    type: "select",
+    data_key: "customers",
+  },
+  {
+    key: "name",
+    placeholder: "Name",
+    type: "text",
+  },
+  // {
+  //     key: 'name',
+  //     placeholder: 'On Market',
+  //     type: 'text',
+  // },
+  {
+    key: "withdrawn",
+    placeholder: "Withdrawn",
+    type: "select",
+    data_key: "withdrawn",
+  },
+  {
+    key: "type",
+    placeholder: "Type",
+    type: "select",
+    data_key: "type",
+  },
 ];
--- src/modules/products/allProducts/components/editProduct/product details/ProductDetails.jsx
+++ src/modules/products/allProducts/components/editProduct/product details/ProductDetails.jsx
@@ -1,140 +1,163 @@
-import { UploadOutlined } from '@ant-design/icons';
+import { WarningOutlined } from "@ant-design/icons";
 import { ButtonComponent } from "@comps/components";
 import { getErrorProps, makeRequest, notify } from "@utils/helpers";
-import { Button, Col, Form, Input, Row, Select, Upload } from "antd";
+import { Col, DatePicker, Form, Input, Row, Select, Switch } from "antd";
 import React, { useState } from "react";
-import { Switch } from 'react-router-dom';
 import { updateSubstance } from "../../request";
-import {WarningOutlined
-    
-  } from '@ant-design/icons';
 
 const ProductDetails = (props) => {
-    const [disabled, setDisabled] = useState(true);
+  const [disabled, setDisabled] = useState(true);
   const toggle = () => {
     setDisabled(!disabled);
   };
+  var selected = {};
 
   const onChange = (checked) => {
     console.log(`switch to ${checked}`);
   };
-  
-    const [loader, setLoader] = useState("");
-    const [errors, setErrors] = useState([]);
 
-    const onFinish = (payload) => {
-        payload.id = props.data.id;
-        makeRequest(setLoader, updateSubstance, payload, onSuccess, onError);
-    };
+  const [loader, setLoader] = useState("");
+  const [errors, setErrors] = useState([]);
 
-    const onSuccess = (data, res) => {
-        notify("Substance", res.msg);
-    };
+  const onFinish = (payload) => {
+    payload.id = props.data.id;
+    makeRequest(setLoader, updateSubstance, payload, onSuccess, onError);
+  };
 
-    const onError = (err) => {
-        let errorList = [];
-        errorList["password"] = err;
-        setErrors(errorList);
-    };
-    const normFile = (e) => {
-        console.log('Upload event:', e);
+  const onSuccess = (data, res) => {
+    notify("Substance", res.msg);
+  };
 
-        if (Array.isArray(e)) {
-            return e;
-        }
+  const onError = (err) => {
+    let errorList = [];
+    errorList["password"] = err;
+    setErrors(errorList);
+  };
+  const normFile = (e) => {
+    console.log("Upload event:", e);
 
-        return e?.fileList;
-    };
+    if (Array.isArray(e)) {
+      return e;
+    }
 
-    return (
-        <Form
-            layout="vertical"
-            // labelCol={{ span: 7 }}
-            initialValues={props.data}
-            onFinish={onFinish}
+    return e?.fileList;
+  };
+
+  return (
+    <Form
+      layout="vertical"
+      // labelCol={{ span: 7 }}
+      initialValues={props.data}
+      onFinish={onFinish}
+    >
+      <Row gutter={[16, 24]}>
+        <Col xs={24} md={24}>
+          <h5>Product Details</h5>
+        </Col>
+
+        <Col className="gutter-row" xs={24} md={12} lg={12}>
+          <Form.Item
+            name="Launch Year"
+            rules={rules.name}
+            label="Product Available for purchase from Date :"
+            {...getErrorProps(errors["name"])}
+          >
+            <DatePicker />
+          </Form.Item>
+        </Col>
+        <Col className="gutter-row" xs={24} md={12} lg={12}>
+          <Form.Item name="customers" label="Customer :">
+            <Select
+              showSearch
+              placeholder="Customer   "
+              options={props.dependencies.customers}
+            />
+          </Form.Item>
+        </Col>
+      </Row>
+
+      <Row gutter={[16, 24]}>
+        <Col xs={24} md={24}>
+          <h5>Product Withdrawn Status</h5>
+        </Col>
+
+        <Col className="gutter-row" xs={24} lg={12}>
+          <Form.Item
+            name="is_active"
+            label="This product is withdrawal from selling :"
+            className="da-mb-16"
+          >
+            <Switch defaultChecked onChange={onChange} />
+          </Form.Item>
+        </Col>
+        <Col className="gutter-row" xs={24} lg={12}>
+          <Form.Item name="product_width" label="Withdrawal Date">
+            <Input disabled={disabled} />
+          </Form.Item>
+        </Col>
+      </Row>
+      <Row>
+        <Col span={12} className="productDetails">
+          <p>
+            {" "}
+            <strong>
+              <WarningOutlined />
+            </strong>
+            This Product is invalid or contains invalid or missing elements
+          </p>
+          <p>
+            {" "}
+            <strong>
+              <WarningOutlined />
+            </strong>
+            This Product hase invalid recepi
+          </p>
+          <p>
+            {" "}
+            <strong>
+              <WarningOutlined />
+            </strong>
+            Product Recipe is incomplete Missing 10%
+          </p>
+          <p>
+            {" "}
+            <strong>
+              <WarningOutlined />
+            </strong>
+            Product Weight is invalid (0mg to 10mg)
+          </p>
+          <p>
+            {" "}
+            <strong>
+              <WarningOutlined />
+            </strong>
+            This required Minimum one emission test
+          </p>
+        </Col>
+      </Row>
+
+      <Form.Item style={{ textAlign: "end" }}>
+        <ButtonComponent
+          className="da-mr-10"
+          type="primary"
+          htmlType="submit"
+          state={loader}
         >
-            <Row gutter={[16, 24]}>
-                <Col  xs={24} md={24}>
-                <h5>Product Details</h5>
-                </Col>
-                
-                <Col className="gutter-row" xs={24} md={12} lg={12} >
-                    <Form.Item
-                        name="name"
-                        rules={rules.name}
-                        label="Product Available for purchase from Date :"
-                        {...getErrorProps(errors["name"])}
-                    >
-                        <Input />
-                    </Form.Item>
-                </Col>
-                <Col className="gutter-row" xs={24} md={12} lg={12}>
-                    <Form.Item name="type_id" label="Customer :">
-                        <Select
-                            showSearch
-                            placeholder="Customer   "
-                            options={props.dependencies.e_types}
-                        />
-                    </Form.Item>
-                </Col>        
-
-            </Row>
-
-            <Row gutter={[16, 24]}>
-                <Col  xs={24} md={24}>
-                <h5>Product Withdrawn Status</h5>
-                </Col>
-                
-                <Col className="gutter-row" xs={24}  lg={12} >
-                <Form.Item
-                name="is_test"
-                label="Small And Medium Size :"
-                className="da-mb-16"
-              >
-                <Switch defaultChecked onChange={onChange} />
-              </Form.Item>
-                </Col>
-                <Col className="gutter-row" xs={24}  lg={12}>
-                <Form.Item name="product_width" label="Liquid Volume Capacity" >
-                    <Input  disabled={disabled}/>
-                  </Form.Item>
-               
-                </Col>
-                  
-
-            </Row>
-            <Row>
-                <Col span={12} className='productDetails'>
-                    <p> <strong><WarningOutlined/></strong>This Product is invalid or contains invalid or missing elements</p>
-                    <p> <strong><WarningOutlined/></strong>This Product hase invalid recepi</p>
-                    <p> <strong><WarningOutlined/></strong>Product Recipe is incomplete Missing 10%</p>
-                    <p> <strong><WarningOutlined/></strong>Product Weight is invalid (0mg to  10mg)</p>
-                    <p> <strong><WarningOutlined/></strong>This required Minimum one emission test</p>
-                </Col>
-            </Row>
-
-            <Form.Item style={{ textAlign: "end" }}>
-                <ButtonComponent
-                    className="da-mr-10"
-                    type="primary"
-                    htmlType="submit"
-                    state={loader}
-                >
-                    Apply
-                </ButtonComponent>
-            </Form.Item>
-        </Form>
-    );
+          Apply
+        </ButtonComponent>
+      </Form.Item>
+    </Form>
+  );
 };
 
 export default ProductDetails;
 
 const rules = {
-    name: [
-        { required: true, message: "Please input your password!" },
-        { min: 3, message: "Minimum password length is 3" },
-        { max: 100, message: "Maximum password length is 100" },
-    ],
-    role_id: [{ required: true, message: "Please select user role!" }],
-    company_id: [{ required: true, message: "Please select user third party!" }],
+  name: [
+    { required: true, message: "Please input your password!" },
+    { min: 3, message: "Minimum password length is 3" },
+    { max: 100, message: "Maximum password length is 100" },
+  ],
+  role_id: [{ required: true, message: "Please select user role!" }],
+  company_id: [{ required: true, message: "Please select user third party!" }],
 };
