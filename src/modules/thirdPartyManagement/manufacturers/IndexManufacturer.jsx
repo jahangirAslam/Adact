06939f7a6a8815992f4c58d154b06e28cd6377aa
jahangirAslam <hafiz.j.aslam@gmail.com>
2022-08-11 10:48:01 +0500

    commit

--- src/modules/thirdPartyManagement/manufacturers/IndexManufacturer.jsx
+++ src/modules/thirdPartyManagement/manufacturers/IndexManufacturer.jsx
@@ -1,208 +1,232 @@
-import React, { useState, useEffect } from "react";
-import { useHistory } from "react-router-dom";
-import { HeaderComponent, BodyComponent, TableComponent, ActionComponent, CreateButton, FilterComponent } from "@comps/components";
-import { makeRequest, removeById, formatCompleteDataTime, notify } from "@utils/helpers";
-import { getManufacturers, deleteManufacturer, getFilters } from "./requests";
-import CreateManufacturer from "./components/CreateManufacturer";
+import {
+  ActionComponent,
+  BodyComponent,
+  CreateButton,
+  FilterComponent,
+  HeaderComponent,
+  SelectionTable,
+} from "@comps/components";
+import { makeRequest, notify, removeById } from "@utils/helpers";
 import { Tag } from "antd";
+import React, { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
+import CreateManufacturer from "./components/CreateManufacturer";
+import { deleteManufacturer, getFilters, getManufacturers } from "./requests";
 
 const pageConfig = {
-    headers: {
-        title: " Manufactures List",
-        breadcrumb: [
-            {
-                name: "Manufacturer",
-                path: "/third-party/manufacturers"
-            }
-        ]
-    }
-}
+  headers: {
+    title: " Manufactures List",
+    breadcrumb: [
+      {
+        name: "Manufacturer",
+        path: "/third-party/manufacturers",
+      },
+    ],
+  },
+};
 
 const IndexManufacturer = () => {
+  var delItems = [];
+  const [loader, setLoader] = useState(false);
+  const history = useHistory();
+  const [filters, setFilters] = useState({});
+  const [dataSource, setDataSource] = useState([]);
+  const [totalRecords, setTotalRecords] = useState(0);
+  const [childComponent, setChildComponent] = useState(null);
+  const [pagination, setPagination] = useState({
+    current: 1,
+    pageSize: 25,
+    sortName: "id",
+    sortType: "desc",
+  });
 
-    const [loader, setLoader] = useState(false);
-    const history = useHistory();
-    const [filters, setFilters] = useState({});
-    const [dataSource, setDataSource] = useState([]);
-    const [totalRecords, setTotalRecords] = useState(0);
-    const [childComponent, setChildComponent] = useState(null);
-    const [pagination, setPagination] = useState({
-        current: 1,
-        pageSize: 25,
-        sortName: 'id',
-        sortType: 'desc'
-    });
+  const columns = [
+    {
+      key: "name",
+      title: "Name",
+      dataIndex: "name",
+      sorter: true,
+    },
+    {
+      key: "country_name",
+      title: "Country",
+      dataIndex: "country_name",
+      sorter: true,
+    },
+    {
+      key: "Email",
+      title: "Email",
+      dataIndex: "Email",
+      sorter: true,
+    },
+    {
+      key: "first_address_line",
+      title: "Address",
+      dataIndex: "first_address_line",
+      sorter: true,
+    },
+    {
+      key: "phone",
+      title: "Contact",
+      dataIndex: "phone",
+      sorter: true,
+    },
+    // {
+    //     key: 'account',
+    //     title: 'Account',
+    //     dataIndex: 'account',
+    //     sorter: true,
+    // },
 
-    const columns = [
-        {
-            key: 'name',
-            title: 'Name',
-            dataIndex: 'name',
-            sorter: true,
-            
-        },
-        {
-            key: 'country_name',
-            title: 'Country',
-            dataIndex: 'country_name',
-            sorter: true,
-        },
-        {
-            key: 'Email',
-            title: 'Email',
-            dataIndex: 'Email',
-            sorter: true,
-        },
-        {
-            key: 'first_address_line',
-            title: 'Address',
-            dataIndex: 'first_address_line',
-            sorter: true,
-        },
-        {
-            key: 'phone',
-            title: 'Contact',
-            dataIndex: 'phone',
-            sorter: true,
-        },
-        // {
-        //     key: 'account',
-        //     title: 'Account',
-        //     dataIndex: 'account',
-        //     sorter: true,
-        // },
+    {
+      key: "status",
+      title: "Status",
+      dataIndex: "is_active",
+      render: (is_active) => {
+        let color = is_active ? "green" : "red";
+        let text = is_active ? "ACTIVE" : "INACTIVE";
+        return <Tag color={color}>{text}</Tag>;
+      },
+    },
+    // {
+    //     key: 'created_at',
+    //     title: 'Create At',
+    //     dataIndex: 'created_at',
+    //     sorter: true,
+    //     render: (created_at) => {
+    //         return formatCompleteDataTime(created_at);
+    //     }
+    // },
+    {
+      key: "actions",
+      title: "Actions",
+      render: (record) =>
+        ActionComponent({ each: record, onEdit: onEdit, onDelete: onDelete }),
+    },
+  ];
 
-        {
-            key: 'status',
-            title: 'Status',
-            dataIndex: 'is_active',
-            render: (is_active) => {
-              let color = is_active ? 'green' : 'red';
-              let text = is_active ? 'ACTIVE' : 'INACTIVE';
-              return (
-                <Tag color={color} >{text}</Tag>
-              );
-            }
-          },
-        // {
-        //     key: 'created_at',
-        //     title: 'Create At',
-        //     dataIndex: 'created_at',
-        //     sorter: true,
-        //     render: (created_at) => {
-        //         return formatCompleteDataTime(created_at);
-        //     }
-        // },
-        {
-            key: "actions",
-            title: 'Actions',
-            render: (record) => ActionComponent({ each: record, onEdit: onEdit, onDelete: onDelete })
-        },
-    ];
+  useEffect(() => {
+    getAllManufacturers();
+    // eslint-disable-next-line
+  }, [pagination, filters]);
 
-    useEffect(() => {
-        getAllManufacturers();
-        // eslint-disable-next-line
-    }, [pagination, filters]);
+  const getAllManufacturers = () => {
+    let payload = {
+      start: pagination.current - 1,
+      length: pagination.pageSize,
+      sort_name: pagination.sortName,
+      sort_type: pagination.sortType,
+      // filters : {"type": "Manufacturers"},
+      filters: { ...filters, type: "manufacture" },
+    };
+    makeRequest(setLoader, getManufacturers, payload, onSuccess, null);
+  };
 
-    const getAllManufacturers = () => {
-        let payload = {
-            start: pagination.current - 1,
-            length: pagination.pageSize,
-            sort_name: pagination.sortName,
-            sort_type: pagination.sortType,
-            // filters : {"type": "Manufacturers"},
-            filters : {...filters,type: "manufacture" },
+  const onSuccess = (response) => {
+    setTotalRecords(response.recordsTotal);
+    setDataSource(response.data);
+  };
 
-        
-        };
-        makeRequest(setLoader, getManufacturers, payload, onSuccess, null);
+  const handleTableChange = (page, fil, sorter) => {
+    let payload = {
+      ...pagination,
+      current: page.current,
+      pageSize: page.pageSize,
+      sortName: sorter.field || "id",
+      sortType: sorter.order === "ascend" ? "asc" : "desc",
+    };
+    setPagination(payload);
+  };
+
+  // Create component modal
+  const onCreate = () => {
+    setChildComponent(<CreateManufacturer onCreated={onCreated} />);
+  };
+
+  const onCreated = (res) => {
+    if (res) {
+      history.push(`/third-party/Manufacturers/edit/${res.data.object.id}`);
     }
+    setChildComponent(null);
+  };
 
-    const onSuccess = (response) => {
-        setTotalRecords(response.recordsTotal);
-        setDataSource(response.data);
-    }
+  const onEdit = (record) => {
+    history.push(`/third-party/Manufacturers/edit/${record.id}`);
+  };
 
-    const handleTableChange = (page, fil, sorter) => {
-        let payload = {
-            ...pagination,
-            current: page.current,
-            pageSize: page.pageSize,
-            sortName: sorter.field || 'id',
-            sortType: sorter.order === 'ascend' ? 'asc' : 'desc',
-        };
-        setPagination(payload);
-    }
-
-    // Create component modal
-    const onCreate = () => {
-       setChildComponent(<CreateManufacturer onCreated={onCreated} />)
-    }
-
-    const onCreated = (res) => {
-        if(res){
-            
-            history.push(`/third-party/Manufacturers/edit/${res.data.object.id}`);
-        }
-        setChildComponent(null);
-    }
-
-    const onEdit = (record) => {
-        history.push(`/third-party/Manufacturers/edit/${record.id}`);
-    }
-
-    const onDelete = (record) => {
-        makeRequest(setLoader, deleteManufacturer, record.id, onDeleteSuccess,
-            onError)
-    }
-
-    const onDeleteSuccess = (response, msg) => {
-        setDataSource(removeById(dataSource, response.id));
-        notify(msg.msg)
-    }
-
-    const onError = (error, msg) => {
-        notify(msg.message)
-    }
-
-    return (
-        <>
-        { childComponent }
-            <HeaderComponent headers={ pageConfig.headers }>
-                <CreateButton onClick={ onCreate } />
-            </HeaderComponent>
-            <BodyComponent>
-            <FilterComponent filters={ availableFilters } onFilter={ setFilters } api={ getFilters } />
-                <TableComponent loader={ loader } columns={ columns } dataSource={ dataSource } pagination={ { ...pagination, total: totalRecords } } onChange={ handleTableChange } />
-            </BodyComponent>
-        </>
+  const onDelete = (record) => {
+    makeRequest(
+      setLoader,
+      deleteManufacturer,
+      record.id,
+      onDeleteSuccess,
+      onError
     );
-}
+  };
+
+  const onDeleteSuccess = (response, msg) => {
+    setDataSource(removeById(dataSource, response.id));
+    notify(msg.msg);
+  };
+
+  const onError = (error, msg) => {
+    notify(msg.message);
+  };
+  const rowSelection = {
+    onChange: (selectedRowKeys) => {
+      delItems = [];
+      delItems = selectedRowKeys;
+    },
+  };
+
+  return (
+    <>
+      {childComponent}
+      <HeaderComponent headers={pageConfig.headers}>
+        <CreateButton onClick={onCreate} />
+      </HeaderComponent>
+      <BodyComponent>
+        <FilterComponent
+          filters={availableFilters}
+          onFilter={setFilters}
+          api={getFilters}
+        />
+        <SelectionTable
+          loader={loader}
+          columns={columns}
+          dataSource={dataSource}
+          pagination={{ ...pagination, total: totalRecords }}
+          rowSelection={rowSelection}
+          onChange={handleTableChange}
+        />
+      </BodyComponent>
+    </>
+  );
+};
 
 export default IndexManufacturer;
 
 const availableFilters = [
-    {
-        key: 'name',
-        placeholder: 'Name',
-        type: 'text',
-    },
-    {
-        key: 'country_id',
-        placeholder: 'Country ',
-        type: 'select',
-        data_key:'country'
-    },
-    {
-        key: 'email',
-        placeholder: 'Email ',
-        type: 'text',
-    },
-    {
-        key: 'status',
-        placeholder: 'Status ',
-        type: 'select',
-        data_key:'status'
-    },
+  {
+    key: "name",
+    placeholder: "Name",
+    type: "text",
+  },
+  {
+    key: "country_id",
+    placeholder: "Country ",
+    type: "select",
+    data_key: "country",
+  },
+  {
+    key: "email",
+    placeholder: "Email ",
+    type: "text",
+  },
+  {
+    key: "status",
+    placeholder: "Status ",
+    type: "select",
+    data_key: "status",
+  },
 ];
